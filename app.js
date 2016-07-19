1a324d61aa639de8edd4379b62defe5423dcfea0
Schutts <jschutts@ms.ds.uhc.com>
2016-07-19 15:07:43 -0400

    fixed apostrophe

--- app.js
+++ app.js
@@ -75,11 +75,11 @@ io.sockets.on('connection', function (socket) {
                 };
 
                 var req = http.request(options, (res) => {
-                    console.log(`STATUS: ${res.statusCode}`);
-                    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+                    console.log('STATUS: ${res.statusCode}');
+                    console.log('HEADERS: ${JSON.stringify(res.headers)}');
                     res.setEncoding('utf8');
                     res.on('data', (chunk) => {
-                        console.log(`BODY: ${chunk}`);
+                        console.log('BODY: ${chunk}');
                     });
                     res.on('end', () => {
                         console.log('No more data in response.')
@@ -87,7 +87,7 @@ io.sockets.on('connection', function (socket) {
                 });
 
                 req.on('error', (e) => {
-                    console.log(`problem with request: ${e.message}`);
+                    console.log('problem with request: ${e.message}');
                 });
 
                 req.write(postData);
