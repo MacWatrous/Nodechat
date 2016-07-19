8d86208622ead9fc06c8422b5f56910e471b4cc3
Schutts <jschutts@ms.ds.uhc.com>
2016-07-19 15:54:58 -0400

    fuck this

--- app.js
+++ app.js
@@ -64,29 +64,29 @@ io.sockets.on('connection', function (socket) {
                 })
 
                 var options = {
-                    /*hostname: 'http://api.api.ai',
+                    hostname: 'http://api.api.ai',
                     port: 80,
                     path: '/v1/entities/drugs/entries',
                     method: 'PUT',
                     headers: {
                         'Authorization': 'Bearer b9c554f76c3b471780436428dd458afd',
                         'Content-Type': 'application/json; charset=utf-8'
-                    }*/                            
+                    }                            
                 };
 
-                var req = http.request(options, (res) => {
+                var req = http.request(options, function(res){
                     console.log('STATUS: ${res.statusCode}');
                     console.log('HEADERS: ${JSON.stringify(res.headers)}');
                     res.setEncoding('utf8');
-                    res.on('data', (chunk) => {
+                    res.on('data', function(chunk) {
                         console.log('BODY: ${chunk}');
                     });
-                    res.on('end', () => {
+                    res.on('end', function() {
                         console.log('No more data in response.')
                     })
                 });
 
-                req.on('error', (e) => {
+                req.on('error', function(e) {
                     console.log('problem with request: ${e.message}');
                 });
 
