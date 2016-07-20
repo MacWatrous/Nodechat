c5442455b9e49767408b7f9ec22b788bfad213e3
macwatrous <mwatrous@ms.ds.uhc.com>
2016-07-20 16:44:26 -0400

    fixing entity add

--- app.js
+++ app.js
@@ -62,8 +62,9 @@ io.sockets.on('connection', function (socket) {
 	            }, function(error, response, body){
 	                //console.log(body);
 	                response=JSON.parse(response);
-	                var synonyms = response.entries[drug[1]].synonyms;
-	                console.log(synonyms);
+	                console.log(response);
+	                //var synonyms = response.entries[drug[1]].synonyms;
+	                //console.log(synonyms);
 	            });
 
 
