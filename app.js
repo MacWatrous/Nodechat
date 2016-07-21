0c92c97ad8edc6067c2e2f9a3fcab60cb3c82ee8
macwatrous <mwatrous@ms.ds.uhc.com>
2016-07-21 10:45:21 -0400

    fixing entities

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ io.sockets.on('connection', function (socket) {
 	               
 	            });
 				console.log(response.entries.length);
-	            console.log(response.entries[1].value)
+	            console.log(response.entries[1].value);
 	            console.log(drug[1]);
 	            //console.log(response);
 	            for (var i=0; i<response.entries.length; i++){
