57348f623c9be0fb123e2de3df27cc7f52c6d825
macwatrous <mwatrous@ms.ds.uhc.com>
2016-07-21 11:53:05 -0400

    fixing entities

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ io.sockets.on('connection', function (socket) {
 	                },
 	                url: 'https://api.api.ai/v1/entities/drug',
 	            }, function(error, response, body){
-	                console.log(body);
+	                //console.log(body);
 	                console.log(drug);
 	                var response2 = body;
 	                console.log(response2.entries.length);
